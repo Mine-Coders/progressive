3c7ea22dfeaf967ca559e52546f27c3f26078192
= <=>
2026-08-16 13:04:34 -0500

    Uncommented out class and updated registration to use the modern mechanism

--- src/main/java/minecoders/progressive/BlockEntities.java
+++ src/main/java/minecoders/progressive/BlockEntities.java
@@ -1,22 +1,22 @@
 package minecoders.progressive;
 
-//import minecoders.progressive.blocks.entities.ExampleBlockEntity;
+import minecoders.progressive.blocks.entities.ExampleBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import org.jetbrains.annotations.NotNull;
 
 public class BlockEntities {
-//    public static final BlockEntityType<ExampleBlockEntity> EXAMPLE = register("example", ExampleBlockEntity::new, Blocks.EXAMPLE);
+    public static final BlockEntityType<ExampleBlockEntity> EXAMPLE = register("example", ExampleBlockEntity::new, Blocks.EXAMPLE);
 
     public static void initialize() {
         Progressive.LOGGER.info("Registering blocks entities...");
     }
 
-//    private static <T extends BlockEntity>
-//    BlockEntityType<T> register(String name, BlockEntityType.BlockEntityFactory<? extends @NotNull T> entityFactory, Block... blocks) {
-//        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Progressive.id(name), BlockEntityType.Builder.<T>create(entityFactory, blocks).build());
-//    }
+    private static <T extends BlockEntity>
+    BlockEntityType<T> register(String name, FabricBlockEntityTypeBuilder.Factory<? extends T>  entityFactory, Block... blocks) {
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Progressive.id(name), FabricBlockEntityTypeBuilder.<T>create(entityFactory, blocks).build());
+    }
 }
