bc9230d6b8dc0da57355f7504eaa3da66b62c3ff
= <=>
2026-08-16 13:05:04 -0500

    Uncommented out class and updated registration to use the modern mechanisms

--- src/main/java/minecoders/progressive/DamageSources.java
+++ src/main/java/minecoders/progressive/DamageSources.java
@@ -1,40 +1,45 @@
 package minecoders.progressive;
 
+import minecoders.progressive.util.helper.RegistryHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageType;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.world.World;
+
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class DamageSources {
-    public static final RegistryKey<DamageType> EXAMPLE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, Progressive.id("stepping"));
+    public static final RegistryKey<DamageType> EXAMPLE = RegistryHelper.getKeyOfDamageType("example");
 
-//    public static DamageSource getExampleDamage(World world) {
-//        return get(world, EXAMPLE);
-//    }
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    public static DamageSource getExampleDamage(World world) {
+        return get(world, EXAMPLE).get();
+    }
 
-//    public static DamageSource getExampleDamage(World world, Entity attacker) {
-//        return get(world, EXAMPLE, attacker);
-//    }
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    public static DamageSource getExampleDamage(World world, Entity attacker) {
+        return get(world, EXAMPLE, attacker).get();
+    }
 
-//    public static DamageSource get(World world, RegistryKey<DamageType> registryKey) {
-//        return new DamageSource(
-//            world.getRegistryManager()
-//                .get(RegistryKeys.DAMAGE_TYPE)
-//                .entryOf(registryKey)
-//        );
-//    }
-//
-//    public static DamageSource get(World world, RegistryKey<DamageType> registryKey, Entity attacker) {
-//        return new DamageSource(
-//            world.getRegistryManager()
-//                .get(RegistryKeys.DAMAGE_TYPE)
-//                .entryOf(registryKey),
-//            attacker
-//        );
-//    }
+    public static Optional<DamageSource> get(World world, RegistryKey<DamageType> registryKey) {
+        Optional<RegistryEntry.Reference<DamageType>> reference = world.getRegistryManager()
+            .getOrThrow(RegistryKeys.DAMAGE_TYPE)
+            .getEntry(registryKey.getValue());
+
+        return reference.map(DamageSource::new);
+    }
+
+    public static Optional<DamageSource> get(World world, RegistryKey<DamageType> registryKey, Entity attacker) {
+        Optional<RegistryEntry.Reference<DamageType>> reference = world.getRegistryManager()
+            .getOrThrow(RegistryKeys.DAMAGE_TYPE)
+            .getEntry(registryKey.getValue());
+
+        return reference.map(damageTypeReference -> new DamageSource(damageTypeReference, attacker));
+    }
 
     public static void initialize() {
         Progressive.LOGGER.debug("Initialized Damage Sources");
